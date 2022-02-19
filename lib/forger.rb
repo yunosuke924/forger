2f458a2988f4e4fc6b711c6e52e92c58f4a289f6
yunosuke924 <y.minegishi0924@gmail.com>
2022-02-19 23:35:21 +0900

    add first commit

---
+++ lib/forger.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "forger/version"
+
+module Forger
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/forger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Forger do
+  it "has a version number" do
+    expect(Forger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
